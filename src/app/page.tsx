bab7d95fd130fe3502f4c1d01b234bd68eabb1c2
nayansi-g <nayansigupta29@gmail.com>
2026-03-16 17:15:03 +0530

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // app/page.tsx
 import type { Metadata } from "next";
 import Hero from "@/components/Hero";
-import TrustStrip from "@/components/TrustStrip";
+// import TrustStrip from "@/components/TrustStrip";
 import IndustriesSection from "@/components/Industries";
 import ServicesSection from "@/components/ServicesSection";
 import CaseStudySection from "@/components/CaseStudySection";
@@ -39,7 +39,7 @@ export default function Home() {
         dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
       />
       <Hero />
-      <TrustStrip />
+      {/* <TrustStrip /> */}
       <IndustriesSection />
       <ServicesSection />
       <CaseStudySection />
